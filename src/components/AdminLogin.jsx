e632757ff2a0aecf561d5b88d28a2fd995f59263
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-10-18 09:54:26 +0530

    login

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -3,26 +3,17 @@ import { VscEye, VscEyeClosed } from 'react-icons/vsc';
 function AdminLogin() {
   
   const [name, setName] = useState('')
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [admin, setAdmin] = useState('')
   const [isSignInForm, setIsSignInForm] = useState(true);
 
-  console.log(username);
-  console.log(password);
+  // console.log(email);
+  // console.log(password);
 
   const handleShowPassword = () => {
     setShowPassword(!showPassword);
-  }
-  
-  const isLogin = () => {
-    if (username==='admin' && password==='1234') {
-      alert('Login Successfully...');
-    }
-    else{
-      alert('Something went wrong...');
-    }
   }
 
   const toggleSignUpForm = () => {
@@ -34,19 +25,30 @@ function AdminLogin() {
   }, [])
 
   const fetchData = async () => {
-    let data = await fetch('http://localhost:3000/Admin');
+    let data = await fetch('http://localhost:8000/Admin');
     let json = await data.json();
     setAdmin(json);
   }
-  console.log(admin);
+  // console.log(admin);
   
+  const isLogin = () => {
+    let login = admin.filter((x) => {
+      return (x.email == email, x.password == password)
+    })
+    if(login.length > 0){
+      alert('Login Successfully...');
+    }
+    else{
+      alert('Something went wrong...');
+    }
+  }
 
   return (
     <div className='w-full h-screen flex justify-center items-center bg-[url("https://wallpapercave.com/wp/wp2939910.jpg")] bg-no-repeat bg-cover bg-center'>
       <form onSubmit={(e) => e.preventDefault()} className="p-12 sm:w-1/2 md:w-3/12 h-[500px] text-white bg-black bg-opacity-50 rounded-lg flex flex-col justify-center items-center">
         <h1 className="font-bold text-3xl py-4 mr-auto">Admin { isSignInForm ? "Sign In" : "Sign Up" }</h1>
         {!isSignInForm && <input type="text" onChange={(e)=>{setName(e.target.value)}} value={name} placeholder="Enter You Name..." className="px-3 py-2 m-2 w-full bg-gray-600 placeholder:font-semibold" required />}
-        <input type="text" onChange={(e)=>{setUsername(e.target.value)}} value={username} placeholder="Enter Your Email..." className="px-3 py-2 my-3 w-full bg-gray-600 placeholder:font-semibold" required />
+        <input type="text" onChange={(e)=>{setEmail(e.target.value)}} value={email} placeholder="Enter Your Email..." className="px-3 py-2 my-3 w-full bg-gray-600 placeholder:font-semibold" required />
         {!showPassword ? <VscEye className={isSignInForm ? "relative top-10 left-[120px] text-xl cursor-pointer" : "absolute top-[355px] ml-[230px] text-xl cursor-pointer" } onClick={handleShowPassword} /> : <VscEyeClosed className={isSignInForm ? "relative top-10 left-[120px] text-xl cursor-pointer" : "absolute top-[355px] ml-[230px] text-xl cursor-pointer" } onClick={handleShowPassword} /> }
         <input type={!showPassword ? "password" : "text"} onChange={(e)=>{setPassword(e.target.value)}} value={password} placeholder="Enter Password..." className="px-3 py-2 my-3 w-full bg-gray-600 placeholder:font-semibold" required />
         <button className="p-3 my-6 bg-red-600 rounded-lg w-full font-semibold text-xl" onClick={isLogin}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { VscEye, VscEyeClosed } from 'react-icons/vsc';
 
 function UserLogin() {
   const [name, setName] = useState('')
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
   const [isSignInForm, setIsSignInForm] = useState(true);
-  console.log(username);
-  console.log(password);
+  const [admin, setAdmin] = useState('')
+  // console.log(email);
+  // console.log(password);
 
   const handleShowPassword = () => {
     setShowPassword(!showPassword);
@@ -18,21 +19,37 @@ function UserLogin() {
     setIsSignInForm(!isSignInForm);
   };
 
-  const isLogin = () => {
-    if (username==='user' && password==='1234') {
-      alert('Login Successfully...');
-    }
-    else{
-      alert('Something went wrong...');
-    }
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    let data = await fetch('http://localhost:8000/User');
+    let json = await data.json();
+    setAdmin(json);
   }
+
+  
+    const isLogin = () => {
+      let login = admin.filter((x) => {
+        return (x.email == email, x.password == password)
+      })
+      
+      if(login.length > 0){
+        alert('Login Successfully...');
+      }
+      else{
+        alert('Something went wrong...');
+      }
+    }
+  
 
   return (
     <div className='w-full h-screen flex justify-center items-center bg-[url("https://images8.alphacoders.com/371/371663.jpg")] bg-no-repeat bg-cover bg-center'>
       <form onSubmit={(e) => e.preventDefault()} className="p-12 sm:w-1/2 md:w-3/12 h-[500px] text-white bg-black bg-opacity-60 rounded-lg flex flex-col justify-center items-center ">
       <h1 className="font-bold text-3xl py-4 mr-auto">User { isSignInForm ? "Sign In" : "Sign Up" }</h1>
       {!isSignInForm && <input type="text" onChange={(e)=>{setName(e.target.value)}} value={name} placeholder="Enter You Name..." className="px-3 py-2 m-2 w-full bg-gray-600 placeholder:font-semibold" required />}
-        <input type="text" onChange={(e)=>{setUsername(e.target.value)}} value={username} placeholder="Enter Your Email..." className="px-3 py-2 my-3 w-full bg-gray-600 placeholder:font-semibold" required />
+        <input type="text" onChange={(e)=>{setEmail(e.target.value)}} value={email} placeholder="Enter Your Email..." className="px-3 py-2 my-3 w-full bg-gray-600 placeholder:font-semibold" required />
         {!showPassword ? <VscEye className={isSignInForm ? "relative top-10 left-[120px] text-xl cursor-pointer" : "absolute top-[355px] ml-[230px] text-xl cursor-pointer" } onClick={handleShowPassword} /> : <VscEyeClosed className={isSignInForm ? "relative top-10 left-[120px] text-xl cursor-pointer" : "absolute top-[355px] ml-[230px] text-xl cursor-pointer" } onClick={handleShowPassword} /> }
         <input type={!showPassword ? "password" : "text"} onChange={(e)=>{setPassword(e.target.value)}} value={password} placeholder="Enter Password..." className="px-3 py-2 my-3 w-full bg-gray-600 placeholder:font-semibold" required />
         <button className="p-3 my-6 bg-red-600 rounded-lg w-full font-semibold text-xl" onClick={isLogin}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
