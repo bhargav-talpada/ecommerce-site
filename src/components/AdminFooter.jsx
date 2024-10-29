8e436879f8d9fd877d299e39eb7bac191fcdc9ff
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-10-29 10:39:26 +0530

    new code

--- src/components/AdminFooter.jsx
+++ src/components/AdminFooter.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function AdminFooter() {
   return (
-    <div>AdminFooter</div>
-  )
+    <div className="AdminFooter">
+      <div className="acd">
+        <h3>Contactus</h3>
+        <p>123456789</p>
+        <big>Email:asdfg@gmail.com</big>
+      </div>
+    </div>
+  );
 }
 
-export default AdminFooter
+export default AdminFooter;
